c23f138f931c7d14916238d39f8672cdba321c98
Giasko0 <dibiame@hotmail.it>
2025-09-05 11:52:19 +0200

    Vers bump

--- html/static/scripts/footer.js
+++ html/static/scripts/footer.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
         </div>
         <div class="footer-section">
           <p>OPLÀ - Portale Online per Prenotare Laboratori e Aule</p>
-          <p>Versione 1.2.1</p>
+          <p>Versione 1.2.2</p>
         </div>
         <div class="footer-section">
           <p>Stai riscontrando problemi?</p>
