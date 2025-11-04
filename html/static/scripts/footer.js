a26d19300330c979e4058d9017aaa108c763dbec
Giasko <dibiame@hotmail.it>
2025-11-04 23:09:04 +0100

    1.7

--- html/static/scripts/footer.js
+++ html/static/scripts/footer.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
         </div>
         <div class="footer-section">
           <p>OPLÀ - Portale Online per Prenotare Laboratori e Aule</p>
-          <p>Versione 1.6</p>
+          <p>Versione 1.7</p>
         </div>
         <div class="footer-section">
           <p>Stai riscontrando problemi?</p>
